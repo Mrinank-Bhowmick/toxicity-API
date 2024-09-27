7427e420a893b5d0c787320cd883e1680bf89296
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-09-28 00:19:19 +0530

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.29.1
      project name = toxicity
      framework = next
      framework cli = create-next-app@14.2.5
      package manager = npm@10.2.3
      wrangler = wrangler@3.78.12
      git = 2.37.3.windows.1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
