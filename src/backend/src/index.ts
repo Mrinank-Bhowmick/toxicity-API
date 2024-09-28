9227b9ac8cae4744df6a354d94be6b1ed1cc6a7c
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-09-29 02:55:56 +0530

    seed file added to populate DB

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,33 +1,14 @@
-import type { VectorizeIndex, Ai } from '@cloudflare/workers-types';
-import { CloudflareVectorizeStore, CloudflareWorkersAIEmbeddings } from '@langchain/cloudflare';
-import { parse } from 'csv-parse/browser/esm';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export interface Env {
-	VECTORIZE_INDEX: VectorizeIndex;
-	AI: Ai;
-}
+const app = new Hono();
 
-export default {
-	async fetch(request, env): Promise<Response> {
-		const embeddings = new CloudflareWorkersAIEmbeddings({
-			binding: env.AI,
-			model: '@cf/baai/bge-base-en-v1.5',
-		});
-		const store = new CloudflareVectorizeStore(embeddings, {
-			index: env.VECTORIZE_INDEX,
-		});
+app.use(cors());
 
-		// const results = await store.similaritySearch('sal', 5);
-		// console.log(JSON.stringify(results, null, 2));
-		// return new Response(JSON.stringify(results), {
-		// 	headers: { 'Content-Type': 'application/json' },
-		// });
+app.post('/', async (c) => {
+	if (c.req.header('Content-Type') !== 'application/json') {
+		return c.json({ error: 'JSON Body Expected' }, { status: 406 });
+	}
+});
 
-		let formData = await request.formData();
-		let csvStream = formData.get('training_data.csv').stream();
-		const records = [];
-		const parser = parse({
-			// CSV options if any
-		});
-	},
-} satisfies ExportedHandler<Env>;
+export default app;
